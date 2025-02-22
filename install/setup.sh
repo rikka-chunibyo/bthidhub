22ef0c0e57314e9ed747b51250561647c715dec5
Rikka <t_rikka@icloud.com>
2025-02-21 23:39:34 -0500

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git --no-install-recommends -y
 
-git clone https://github.com/Dreamsorcerer/bthidhub --depth 1
+git clone https://github.com/rikka-chunibyo/bthidhub --depth 1
 cd $HOME/bthidhub/install/on_rpi
 bash ./on_pi_setup.sh
