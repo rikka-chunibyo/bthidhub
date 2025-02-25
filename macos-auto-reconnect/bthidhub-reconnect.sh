cec50fe95d55f897d622fad08ee9986e4dabfbc3
Rikka <t_rikka@icloud.com>
2025-02-25 16:16:02 -0500

    Create bthidhub-reconnect.sh

---
+++ macos-auto-reconnect/bthidhub-reconnect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export BLUEUTIL_ALLOW_ROOT=1
+DEVICE_MAC="e4-5f-01-2a-fb-23"
+DELAY=10
+
+while true; do
+    if [[ $(blueutil --is-connected "$DEVICE_MAC") -eq 0 ]]; then
+        echo "Device not connected. Reconnecting..."
+        blueutil --connect "$DEVICE_MAC"
+    else
+        echo "Device is already connected."
+    fi
+    sleep $DELAY
+done
