9d618bbd552b396462fff61caf7f1b1c92c03adc
Rikka <t_rikka@icloud.com>
2025-02-25 16:04:43 -0500

    Update install.sh

--- macos-auto-reconnect/install.sh
+++ macos-auto-reconnect/install.sh
@@ -1,9 +1,5 @@
 sudo rm -rf "/Library/Startup Scripts/bthidhub-reconnect.sh"
 sudo mkdir "/Library/Startup Scripts"
-
-sudo curl "https://raw.githubusercontent.com/rikka-chunibyo/bthidhub/refs/heads/main/macos-auto-reconnect/Library/LaunchDaemons/com.rikka.bthidhub-reconnect.plist" -o "/Library/LaunchDaemons/com.system.bthidhub-reconnect.plist"
 sudo curl "https://raw.githubusercontent.com/rikka-chunibyo/bthidhub/refs/heads/main/macos-auto-reconnect/Library/Startup%20Scripts/bthidhub-reconnect.sh" -o "/Library/Startup Scripts/bthidhub-reconnect.sh"
-
 sudo chmod +x "/Library/Startup Scripts/bthidhub-reconnect.sh"
-
 (crontab -l 2>/dev/null; echo "@reboot /Library/Startup\\ Scripts/bthidhub-reconnect.sh") | crontab -
